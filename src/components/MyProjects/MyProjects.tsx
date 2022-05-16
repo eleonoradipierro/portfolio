891f8feb353e52163365a193cc2571002be91171
Eleonora Di Pierro <eleonora.dipierro@gmail.com>
2022-05-16 18:23:11 +0200

    better card style

--- src/components/MyProjects/MyProjects.tsx
+++ src/components/MyProjects/MyProjects.tsx
@@ -1,32 +1,72 @@
 import React from "react";
 import styles from "./MyProjects.module.scss";
-import Image from "next/image";
 
-const Card = (props: { name: string; image: string; description: string }) => {
-  const { name, image, description } = props;
+import appleClone from "../../../public/images/apple-clone.png";
+import pokedex from "../../../public/images/pokedex.png";
+
+import { useState } from "react";
+import { SassColor } from "sass";
+
+type StaticImageData = {
+  src: string;
+  height: number;
+  width: number;
+  placeholder?: string;
+};
+
+const Card = (props: {
+  name: string;
+  description: string;
+  image: StaticImageData;
+  src: string;
+}) => {
+  const { name, description, image, src } = props;
+
   return (
-    <div className={styles.projects__cards__item}>
-      <div className={styles.projects__cards__item__title}>{name}</div>
-      <div>
-        {/* <Image src={image} alt={"project" + name} loading="lazy" layout="fill" className={styles.cards__item__image} /> */}
+    <a href={src} className={styles.item}>
+      <img src={image.src} alt={name} className={styles.item__image} />
+      <div className={styles.item__container}>
+        <div className={styles.item__container__title}>{name}</div>
+        <p className={styles.item__container__description}>{description}</p>
       </div>
-      <div className={styles.projects__cards__item__description}>
-        {description}
-      </div>
-    </div>
+    </a>
   );
 };
 
 const MyProjects = () => {
+  const projects = [
+    {
+      name: "Apple UK Homepage Clone",
+
+      description: "Made from scratch apple.com/uk.",
+      src: "https://appleuk-clone-homepage.netlify.app/",
+      image: appleClone,
+    },
+    {
+      name: "Pokedex",
+      description:
+        "Website that allows the user to search data about a Pokemon.",
+      src: "",
+      image: pokedex,
+    },
+  ];
   return (
-    <div className={styles.projects}>
+    <div className={styles.projects} id="projects">
       <div className={styles.projects__title}>
         <h1>Projects</h1>
       </div>
 
       <div className={styles.projects__cards}>
-        <Card name="Apple" image="" description="" />
-        <Card name="Pokemon" image="" description="" />
+        {projects.map((project) => {
+          return (
+            <Card
+              name={project.name}
+              description={project.description}
+              src={project.src}
+              image={project.image}
+            />
+          );
+        })}
       </div>
     </div>
   );
